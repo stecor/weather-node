30bc6b9503a1a94fd17657fb9a335c5f95220ee6
Stefano Corra <stefano.corra@hotmail.com>
2022-12-14 19:17:14 -0500

    node commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
-
+const https = require('https')
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Server is up and running.')
+})
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid='
+
+https.get(url, function (response) {
+  console.log(response.statusCode)
 })
 
 app.listen(3000, function () {
