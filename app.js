2827f7939ffedf0b9347d76874a5bab98df11569
Stefano Corra <stefano.corra@hotmail.com>
2022-12-16 22:28:43 -0500

    node commit

--- app.js
+++ app.js
@@ -3,50 +3,50 @@ const https = require('https')
 const app = express()
 require('dotenv').config()
 const apiKey = process.env.KEY
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+app.post('/', function (req, res) {
+  const query = req.body.cityName
+  const apiKey = '31811e523102e535fa75180f620b9dda'
+  const unit = 'metric'
   const url =
-    'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + apiKey
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    query +
+    '&appid=' +
+    apiKey +
+    '&units=' +
+    unit
 
   https.get(url, function (response) {
-    console.log(response.statusCode)
+    //console.log(response.statusCode)
 
-    //res.sendFile(__dirname + '/index.html')
+    response.on('data', function (data) {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+      res.setHeader('Content-Type', 'text/html')
+
+      res.write(`<h3>The weather is currently ` + weatherDescription + `</h3>`)
+      res.write(
+        `<h1>The temperature in ` +
+          query +
+          ` is ` +
+          temp +
+          ` degrees Celsius </h1>`
+      )
+      res.write('<image src=' + imageUrl + '>')
+      res.send()
+    })
   })
 })
-//app.post('/', function (req, res) {})
-
-// const query = 'London'
-// const apiKey = '31811e523102e535fa75180f620b9dda'
-// const unit = 'metric'
-// const url =
-//   'https://api.openweathermap.org/data/2.5/weather?q=' +
-//   query +
-//   '&appid=' +
-//   apiKey +
-//   '&units=' +
-//   unit
-
-// https.get(url, function (response) {
-//   console.log(response.statusCode)
-
-//   response.on('data', function (data) {
-//     const weatherData = JSON.parse(data)
-//     const temp = weatherData.main.temp
-//     const weatherDescription = weatherData.weather[0].description
-//     const icon = weatherData.weather[0].icon
-//     const imageUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
-
-//     res.setHeader('Content-Type', 'text/html')
-
-//     res.write(`<h3>The weather is currently ` + weatherDescription + `</h3>`)
-//     res.write(
-//       `<h1>The temperature in London is ` + temp + ` degrees Celsius </h1>`
-//     )
-//     res.write('<image src=' + imageUrl + '>')
-//     res.send()
-//   })
-// })
 
 // 3000 is the port number in my case.
 app.listen(3000, function () {
